0a9b252b5bd2995aa652df96f8b347b9376a7d3e
Kraft_ <kraftmajnmail@gmail.com>
2020-02-19 23:28:50 +0100

    merge done

---
+++ Giera/Assets/Scripts/Systems/MovementSystem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Logics.Systems
+{
+    public class MovementSystem : ISystem
+    {
+        private readonly float SPEED_DECREASE = 0.001f;
+        private readonly float INCREMENT_HOLE_SIZE = 0.0001f;
+
+        public void Update(Boat boat)
+        {
+            float sumOfHoleSizes = 0;
+
+            foreach (Hole hole in boat.HolesHull)
+            {
+                hole.Size += (hole.Size * INCREMENT_HOLE_SIZE);
+                sumOfHoleSizes += hole.Size;
+            }
+
+            boat.Speed = 1 - (SPEED_DECREASE * sumOfHoleSizes);
+        }
+    }
+}
---
+++ Giera/Assets/Scripts/WaterLayer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class WaterLayer : MonoBehaviour
+{
+    public string playerTag = "Player";
+    private PlayerScript player;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag(playerTag))
+        {
+            player.canCollectWater = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag(playerTag))
+        {
+            player.canCollectWater = false;
+        }
+    }
+    void Start()
+    {
+        player = FindObjectOfType<PlayerScript>();
+    }
+}
