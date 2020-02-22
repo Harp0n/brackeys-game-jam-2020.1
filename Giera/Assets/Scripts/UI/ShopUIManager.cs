9296943870a69b5c98dc37b1d634ca03a6381e23
Sebastian Pięta <44878991+KraftUnderscore@users.noreply.github.com>
2020-02-23 00:39:37 +0100

    shop UI

---
+++ Giera/Assets/Scripts/UI/ShopUIManager.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class ShopUIManager : MonoBehaviour
+{
+    public string cargoSurfix;
+    public string speedDecreaseSurfix;
+    public string moneySurfix;
+    public TextMeshProUGUI cargoTxt;
+    public TextMeshProUGUI speedDecreaseTxt;
+    public TextMeshProUGUI costTxt;
+    public TextMeshProUGUI moneyTxt;
+    public Slider cargoSlider;
+    public GameObject confirmButton;
+
+    private int selectedCargo;
+    private float costPerCargo = 0.1f;
+    private float speedDecreasePerCargo = 0.2f;
+    private float playerMoney = 2;
+    private bool enoughMoney = true;
+
+    //przypisac kase gracza tutaj - moze z playerpref korzystajac?
+    private void Awake()
+    {
+       // playerMoney = PlayerPrefs.GetFloat("playerMoney", 0f);
+    }
+
+    private void Start()
+    {
+        UpdateUI();
+    }
+
+    //dla slidera funkcja
+    public void UpdateCargo()
+    {
+        selectedCargo = (int)cargoSlider.value;
+        enoughMoney = selectedCargo * costPerCargo <= playerMoney;
+        UpdateUI();
+    }
+    //to modyfikowac z zewnatrz
+    public void SetMinCargo(int minCargo)
+    {
+        cargoSlider.minValue = minCargo;
+    }
+
+    public void SetMaxCargo(int maxCargo)
+    {
+        cargoSlider.maxValue = maxCargo;
+    }
+
+    public void SetCostPerCargo(float costPerCargo)
+    {
+        this.costPerCargo = costPerCargo;
+    }
+
+    public void SetSpeedDecreasePerCargo(float decreasePerCargo)
+    {
+        speedDecreasePerCargo = decreasePerCargo;
+    }
+
+    //wezwana przy pomyslnym zakonczeniu transakcji
+    public void ConfirmCargo()
+    {
+        float totalCost = selectedCargo * costPerCargo;
+        float totalSlowdown = selectedCargo * speedDecreasePerCargo;
+    }
+
+    private void UpdateUI()
+    {
+        confirmButton.SetActive(enoughMoney);
+        if (!enoughMoney) costTxt.color = Color.red;
+        else costTxt.color = Color.black;
+        cargoTxt.SetText(selectedCargo + cargoSurfix);
+        speedDecreaseTxt.SetText(selectedCargo * speedDecreasePerCargo + speedDecreaseSurfix);
+        costTxt.SetText(selectedCargo * costPerCargo + moneySurfix);
+        moneyTxt.SetText(playerMoney + moneySurfix);
+    }
+}
